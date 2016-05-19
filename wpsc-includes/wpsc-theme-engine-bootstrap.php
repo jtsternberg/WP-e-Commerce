d451b521379ba3634f975fd04446da13355e00d5
Justin Sternberg <justin@dsgnwrks.pro>
2016-05-19 11:03:30 -0400

    Address php warning/error related to switching theme engines. Fixes #2084
    Solves:
    
    PHP Warning:  call_user_func_array() expects parameter 1 to be a valid
    callback, function '_wpsc_action_flush_rewrite_rules' not found or
    invalid function name in
    /srv/www/wpecommerce/htdocs/wp-includes/plugin.php on line 525
    
    PHP Fatal error:  Cannot redeclare wpsc_is_checkout() (previously
    declared in
    /srv/www/wpecommerce/htdocs/wp-content/plugins/wp-e-commerce/wpsc-components/theme-engine-v2/helpers/conditional-tags.php:100)
    in
    /srv/www/wpecommerce/htdocs/wp-content/plugins/wp-e-commerce/wpsc-components/theme-engine-v1/helpers/template.php
    on line 31

--- wpsc-includes/wpsc-theme-engine-bootstrap.php
+++ wpsc-includes/wpsc-theme-engine-bootstrap.php
@@ -203,6 +203,9 @@ function _wpsc_maybe_activate_theme_engine_v2() {
 
 	if ( $old_theme_engine !== $new_theme_engine ) {
 
+		// _wpsc_action_flush_rewrite_rules lives here.
+		require_once( WPSC_FILE_PATH . '/wpsc-admin/init.php' );
+
 		do_action( 'wpsc_updated_theme_engine', $new_theme_engine, $old_theme_engine );
 
 		do_action( "wpsc_updated_theme_engine_from_{$old_theme_engine}_to_{$new_theme_engine}" );
@@ -210,6 +213,10 @@ function _wpsc_maybe_activate_theme_engine_v2() {
 		add_action( 'shutdown', '_wpsc_action_flush_rewrite_rules' );
 
 		update_option( 'wpsc_get_active_theme_engine', $new_theme_engine );
+
+		// Redirect ensures that both templates will not be loaded, fixing fatal errors, etc.
+		wp_redirect( esc_url_raw( remove_query_arg( 'blah' ) ) );
+		exit;
 	}
 
 	return $activate;
