44d986c1f99d2a839bdff389e0419c693f3ff597
Justin Sternberg <justin@dsgnwrks.pro>
2017-03-10 00:52:07 -0500

    Move implementation out of FloatLabel.js

--- wpsc-components/theme-engine-v2/theming/assets/js/checkout.js
+++ wpsc-components/theme-engine-v2/theming/assets/js/checkout.js
@@ -1,4 +1,6 @@
 ;(function($) {
+	$('body').addClass( 'wpsc-float-js' );
+
 	$( '.wpsc-field-textfield' ).FloatLabel();
 	$( '.wpsc-field-select_region' ).FloatLabel();
 	$( '.wpsc-field-select_country' ).FloatLabel();
